9c8ae5b173c6c0d70e4f419169eebaf6e059ed98
Syed Areeb <sareeb65@gmail.com>
2023-05-01 17:46:39 +0500

    store

--- ai_summarizer/src/components/Demo.jsx
+++ ai_summarizer/src/components/Demo.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useState } from "react";
+import { linkIcon } from "../assets";
+
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: ''
+  })
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+
+  const handleSubmit = async (e) => {
+    const { data } = await getSummary({
+      articleUrl: article.url
+    })
+  }
+
   return (
-    <div>Demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form action="" onSubmit={handleSubmit} className="relative flex justify-center items-center">
+          <img src={linkIcon} className="absolute left-0 my-2 ml-3 w-5" alt="link_icon" />
+          <input type="url" value={article.url} placeholder="Enter a URL" onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer" />
+          <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"><p>↵</p></button>
+        </form>
+
+      </div>
+
+    </section>
   )
 }
 
