580021a967f12302e71533ffdfb46ac398f194a9
Syed Areeb <sareeb65@gmail.com>
2023-06-12 11:27:45 +0500

    store

--- ai_summarizer/src/components/Demo.jsx
+++ ai_summarizer/src/components/Demo.jsx
@@ -9,12 +9,24 @@ const Demo = () => {
     summary: ''
   })
 
+
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
 
+
+
   const handleSubmit = async (e) => {
-    const { data } = await getSummary({
-      articleUrl: article.url
-    })
+    e.preventDefault()
+    console.log('useLazyGetSummaryQuery', useLazyGetSummaryQuery)
+    console.log('article', article)
+    console.log('getSummary', getSummary)
+    console.log('error', error)
+    console.log('isFetching', isFetching)
+    const { data } = await getSummary({ articleUrl: article.url })
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary }
+      setArticle(newArticle)
+      console.log('newArticle',newArticle)
+    }
   }
 
   return (
@@ -24,11 +36,14 @@ const Demo = () => {
         <form action="" onSubmit={handleSubmit} className="relative flex justify-center items-center">
           <img src={linkIcon} className="absolute left-0 my-2 ml-3 w-5" alt="link_icon" />
           <input type="url" value={article.url} placeholder="Enter a URL" onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer" />
-          <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"><p>↵</p></button>
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+          >
+            <p>↵</p>
+          </button>
         </form>
-
       </div>
-
     </section>
   )
 }
