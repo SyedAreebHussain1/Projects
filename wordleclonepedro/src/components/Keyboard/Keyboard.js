1c5244ca4a5cd48017ddc73836554362264ba155
Syed Areeb <sareeb65@gmail.com>
2023-06-27 19:36:22 +0500

    some changes

--- wordleclonepedro/src/components/Keyboard/Keyboard.js
+++ wordleclonepedro/src/components/Keyboard/Keyboard.js
@@ -1,21 +1,34 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useContext, useEffect } from 'react'
+import { AppContext } from '../../App';
 import Key from '../../utils/Key';
 const Keyboard = () => {
+    const { onSelectLetter, onDelete, onEnter } = useContext(AppContext)
+
     const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
     const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
     const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
     const handleKeyboard = useCallback((event) => {
-        console.log(event)
-        // if (event.key === "Enter") {
-
-        // } else if (event.key === "Backspace") {
-
-        // } else {
-        // }
-        // console.log(event.key)
-    })
-    keys1.forEach((arr) => {
-        console.log(arr)
+        if (event.key === "Enter") {
+            onEnter()
+        } else if (event.key === "Backspace") {
+            onDelete()
+        } else {
+            keys1.forEach((key) => {
+                if (event.key.toLowerCase() === key.toLowerCase()) {
+                    onSelectLetter(key)
+                }
+            })
+            keys2.forEach((key) => {
+                if (event.key.toLowerCase() === key.toLowerCase()) {
+                    onSelectLetter(key)
+                }
+            })
+            keys3.forEach((key) => {
+                if (event.key.toLowerCase() === key.toLowerCase()) {
+                    onSelectLetter(key)
+                }
+            })
+        }
     })
     useEffect(() => {
         document.addEventListener('keydown', handleKeyboard)
